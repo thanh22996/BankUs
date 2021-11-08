a23c11df80c90730b4fb93fe999d3c77600aa477
Thành Nguyễn Sinh <thanhns@Ggroup.vn>
2021-11-08 13:22:38 +0700

    add new 1:22 08/11/2021

---
+++ layout/Footer.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export function Footer() {
+    return (
+        <div className="block-footer">
+            <div className="container">
+            <div className="row">
+                <div className="col-md-3">logo</div>
+                <div className="col-md-3">address</div>
+                <div className="col-md-3">hostline</div>
+                <div className="col-md-3">map</div>
+            </div>
+            </div>
+        </div>
+    )
+}
--- layout/MasterLayout.js
+++ layout/MasterLayout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Footer } from './Footer';
 import Header from './Header';
 
 function MasterLayout(props) {
@@ -6,6 +7,7 @@ function MasterLayout(props) {
         <div className="wrapper-layout">
             <Header />
             {props.children}
+            <Footer />
         </div>
     );
 }
